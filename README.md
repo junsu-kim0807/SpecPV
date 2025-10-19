d8c56524fa7aef458f37c396730fd3e7f04dd149
Tan Zhendong <tanbabydong@gmail.com>
2025-10-19 20:12:50 +0800

    [mod] info

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118 pytorch 2.7.1 cu118
 pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cu118
 
-pip install -e . [dev]
+pip install -e .[dev]
 
 问题检查
 ruff check . 
--- profile/profile_draft_verify.py
+++ profile/profile_draft_verify.py
@@ -8,7 +8,7 @@ EAGLE_model_path = '/home/lab6033/zhendong/models/eagle/EAGLE3-LLaMA3.1-Instruct
 model = Speculator.from_pretrained(
     base_model_path=base_model_path,
     ea_model_path=EAGLE_model_path,
-    torch_dtype=torch.float16,
+    torch_dtype=torch.bfloat16,
     low_cpu_mem_usage=True,
     device_map="auto",
     total_token=-1
@@ -20,10 +20,10 @@ for seqlen in [1024, 4096, 8192, 16384, 32768]:
     input_ids = torch.randint(low=0, high=vocab_size-1000, size=(1, seqlen), device="cuda")
     # warmup
     for _ in range(5):
-        _ = model.ea_generate(input_ids, temperature=0.8, max_new_tokens=128, max_length=40000)
-    output = model.ea_generate(input_ids,temperature=0.8, max_new_tokens=1024, max_length=40000)
+        _ = model.spec_generate(input_ids, temperature=0.8, max_new_tokens=128, max_length=35000)
+    output = model.spec_generate(input_ids,temperature=0.8, max_new_tokens=1024, max_length=35000)
     # get results
-    from sparsesd.speculate.utils import print_time_stats,reset_time_stats
+    from sparsesd.speculate.profile import print_time_stats,reset_time_stats
     print("########### seqlen:", seqlen, "###########")
     print_time_stats()
     reset_time_stats()
